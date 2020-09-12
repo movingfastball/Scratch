9f055b7740b3691a62e8ae4f1b25a5330506b84f
movingfastball <movingfastballny2012@yahoo.co.jp>
2020-09-13 02:29:33 +0900

    フルスクラッチコミット1回目

---
+++ staff_add.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>NY_scratch</title>
+</head>
+<body>
+
+</body>
+</html>
+
+スタッフ追加<br/><br/>
+<form method="POST"action="staff_add_chack.php">
+スタッフ名を入力してください。<br/>
+<input type="text" name="name" style="width:100px"><br/><br/>
+パスワードを入力してください
+<input type="password" name="pass" style="width:100px"><br/>
+パスワードをもう1度入力してください。
+<input type="password" name="pass2" style="width:100px"><br/>
+<br/>
+<input type="button" onclick="history.back()"value="戻る">
+<input type="submit" value="OK">
+</form>
+
+
